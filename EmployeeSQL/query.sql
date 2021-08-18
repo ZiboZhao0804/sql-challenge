4356c58ba4df1dea70cc9be6d02181abac571958
ZiboZhao <zibo.zhao@northwestern.edu>
2021-08-18 17:00:30 -0500

    update script

---
+++ EmployeeSQL/query.sql
@@ -0,0 +1,60 @@
+-- 1.List the following details of each employee: employee number, last name, first name, sex, and salary.
+SELECT e.emp_no,e.last_name,e.first_name,e.sex,s.salary 
+FROM employees AS e
+JOIN salaries AS s
+ON e.emp_no = s.emp_no;
+
+-- 2.List first name, last name, and hire date for employees who were hired in 1986.
+SELECT first_name,last_name,hire_date
+FROM employees 
+WHERE hire_date like '%/1986';
+
+-- 3.List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+SELECT d.dept_no, d.dept_name, dm.emp_no, e.last_name, e.first_name
+FROM departments AS d
+JOIN dept_manager AS dm
+ON d.dept_no = dm.dept_no
+JOIN employees AS e
+ON dm.emp_no = e.emp_no
+ORDER BY dept_no;
+
+-- 4.List the department of each employee with the following information: employee number, last name, first name, and department name.
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees AS e
+JOIN dept_manager AS dm
+ON e.emp_no = dm.emp_no
+JOIN departments AS d
+ON dm.dept_no = d.dept_no;
+
+-- 5.List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+SELECT first_name,last_name,sex
+FROM employees 
+WHERE first_name = 'Hercules' AND last_name like 'B%';
+
+-- 6.List all employees in the Sales department, including their employee number, last name, first name, and department name.
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees AS e
+JOIN dept_manager AS dm
+ON e.emp_no = dm.emp_no
+JOIN departments AS d
+ON dm.dept_no = d.dept_no
+WHERE dept_name = 'Sales';
+
+-- 7.List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees AS e
+JOIN dept_manager AS dm
+ON dm.emp_no =e.emp_no
+JOIN departments AS d
+ON d.dept_no = dm.dept_no
+WHERE dept_name IN ('Sales','Development');
+
+-- 8.In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+SELECT last_name,count(last_name) AS "count"
+FROM employees
+GROUP BY last_name
+ORDER BY "count" DESC;
+
+
+
+
