4ac2ab0d786fdb13db2720235b1b317549510361
ZiboZhao <zibo.zhao@northwestern.edu>
2021-08-20 00:06:29 -0500

    update schema

--- EmployeeSQL/schema.sql
+++ EmployeeSQL/schema.sql
@@ -1,28 +1,24 @@
-DROP TABLE IF EXISTS departments;
-DROP TABLE IF EXISTS dept_emp;
-DROP TABLE IF EXISTS dept_manager;
-DROP TABLE IF EXISTS employees;
 DROP TABLE IF EXISTS salaries;
+DROP TABLE IF EXISTS dept_manager;
+DROP TABLE IF EXISTS dept_emp;
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS departments;
 DROP TABLE IF EXISTS titles;
 
 CREATE TABLE departments (
-	dept_no CHAR(4) NOT NULL,
+	dept_no CHAR(4) NOT NULL PRIMARY KEY,
 	dept_name VARCHAR NOT NULL
 );
 
-CREATE TABLE dept_emp (
-	emp_no INT NOT NULL,
-	dept_no CHAR(4) NOT NULL
-);
-
-CREATE TABLE dept_manager (
-	dept_no CHAR(4) NOT NULL,
-	emp_no INT NOT NULL	
+CREATE TABLE titles (
+	title_id CHAR(5) NOT NULL PRIMARY KEY,
+	title VARCHAR NOT NULL	
 );
 
 CREATE TABLE employees (
-	emp_no INT NOT NULL,
+	emp_no INT NOT NULL PRIMARY KEY,
 	emp_title_id CHAR(5) NOT NULL,
+	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id),
 	birth_date VARCHAR NOT NULL,
 	first_name VARCHAR NOT NULL,
 	last_name VARCHAR NOT NULL,
@@ -31,11 +27,23 @@ CREATE TABLE employees (
 );
 
 CREATE TABLE salaries (
-	emp_no INT NOT NULL,
+	emp_no INT NOT NULL PRIMARY KEY,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
 	salary INT NOT NULL
 );
 
-CREATE TABLE titles (
-	title_id CHAR(5) NOT NULL,
-	title VARCHAR NOT NULL	
+CREATE TABLE dept_emp (
+	emp_no INT NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	dept_no CHAR(4) NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	PRIMARY KEY (emp_no,dept_no)
+);
+
+CREATE TABLE dept_manager (
+	dept_no CHAR(4) NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	emp_no INT NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	PRIMARY KEY (dept_no,emp_no)
 );
